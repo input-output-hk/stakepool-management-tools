1a4b46e87aff21090afaa48578761fefe0783968
lgobbi <lgobbi@atixlabs.com>
2019-11-21 16:48:52 -0300

    SPM-21 - Add Fragment logs data and pagination

--- electron-app/src/components/molecules/ReportTool/FragmentLogs/FragmentLogs.jsx
+++ electron-app/src/components/molecules/ReportTool/FragmentLogs/FragmentLogs.jsx
@@ -1,65 +1,146 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import PropTypes from 'prop-types';
-import { Input } from 'antd';
+import { Input, Icon } from 'antd';
+import moment from 'moment';
 import ButtonPrimary from '../../../atoms/ButtonPrimary/ButtonPrimary';
 import { getMessage } from '../../../../utils/messages';
 import TableFragments from './TableFragments';
+import { getFragmentLogs } from '../../../../utils/api';
+import { formatDateTimeWithComma } from '../../../../utils/formatters';
 
-const FragmentLogs = ({
-  fragments,
-  currentPage,
-  totalPages,
-  onSearch,
-  onBackPage,
-  onNextPage
-}) => (
-  <div className="card">
-    <div className="titleCard4">
-      <h4>{getMessage('report.fragments.title')}</h4>
-      <Input placeholder={getMessage('report.fragments.search.placeholder')} />
-      <ButtonPrimary
-        text={getMessage('report.fragments.search.update')}
-        onClick={onSearch}
-      />
-    </div>
-    <TableFragments fragments={fragments} />
-    <div className="bottomBar">
-      <ButtonPrimary
-        text={getMessage('report.fragments.pagination.back')}
-        onClick={onBackPage}
-      />
-      <p>
-        {getMessage('report.fragments.pagination.current')}
-        {currentPage}
-        {getMessage('report.fragments.pagination.total')}
-        {totalPages}
-      </p>
-      <ButtonPrimary
-        text={getMessage('report.fragments.pagination.next')}
-        onClick={onNextPage}
-      />
-    </div>
-  </div>
-);
+const FragmentLogs = ({ nodeAddress }) => {
+  const PAGE_SIZE = 8;
 
-FragmentLogs.defaultProps = {
-  fragments: []
+  const [fragmentLogs, setFragmentLogs] = useState();
+  const [inputFragmentId, setInputFragmentId] = useState();
+  const [errorMessage, setErrorMessage] = useState(
+    getMessage('report.fragments.missing')
+  );
+  const [currentPage, setCurrentPage] = useState(1);
+  const [totalPages, setTotalPages] = useState(1);
+
+  const fetchData = async () => {
+    if (nodeAddress && nodeAddress !== '') {
+      const fragments = await getFragmentLogs(nodeAddress);
+      const foundFragments = findFragments(fragments);
+      setFragmentLogs(foundFragments);
+    } else {
+      setFragmentLogs(undefined);
+    }
+  };
+
+  const findFragments = fragments => {
+    if (!inputFragmentId || inputFragmentId === '') {
+      setErrorMessage(getMessage('report.fragments.missing'));
+      return;
+    }
+
+    const filteredFragments = fragments.filter(
+      fragment => fragment.fragment_id === inputFragmentId
+    );
+    const sortedFragments = filteredFragments.sort(
+      (a, b) =>
+        moment(b.last_updated_at).format('YYYYMMDDHHmmss') -
+        moment(a.last_updated_at).format('YYYYMMDDHHmmss')
+    );
+
+    const formattedFragments = sortedFragments.map(fragment => {
+      const fragmentId = fragment.fragment_id;
+      const receivedAt = formatDateTimeWithComma(fragment.received_at);
+      const updatedAt = formatDateTimeWithComma(fragment.last_updated_at);
+      let { status } = fragment;
+
+      if (status && status.Rejected) {
+        status = status.Rejected.reason;
+      } else if (status && status.InABlock) {
+        status = status.InABlock.date;
+      }
+
+      return {
+        fragmentId,
+        receivedAt,
+        updatedAt,
+        status
+      };
+    });
+
+    if (formattedFragments && formattedFragments.length <= 0)
+      setErrorMessage(getMessage('report.fragments.notfound'));
+    return formattedFragments;
+  };
+
+  const goToNextPage = () => {
+    if (currentPage < totalPages) setCurrentPage(currentPage + 1);
+  };
+
+  const goToPreviousPage = () => {
+    if (currentPage > 1) setCurrentPage(currentPage - 1);
+  };
+
+  useEffect(() => {
+    fetchData(nodeAddress);
+  }, [nodeAddress]);
+
+  useEffect(() => {
+    if (fragmentLogs && fragmentLogs.length > 0) {
+      setTotalPages(Math.ceil(fragmentLogs.length / PAGE_SIZE))
+    } else {
+      setTotalPages(1);
+    }
+  }, [fragmentLogs]);
+
+  const beginningArray = (currentPage - 1) * PAGE_SIZE;
+
+  return (
+    <div className="card">
+      <div className="titleCard4">
+        <h4>{getMessage('report.fragments.title')}</h4>
+        <Input
+          placeholder={getMessage('report.fragments.search.placeholder')}
+          onChange={({ target: { value } }) => setInputFragmentId(value)}
+        />
+        <ButtonPrimary
+          text={getMessage('report.fragments.search.update')}
+          onClick={fetchData}
+        />
+      </div>
+      {fragmentLogs && fragmentLogs.length > 0 ? (
+        <TableFragments
+          fragments={fragmentLogs.slice(
+            beginningArray,
+            fragmentLogs.length - beginningArray < PAGE_SIZE
+              ? fragmentLogs.length
+              : PAGE_SIZE
+          )}
+        />
+      ) : (
+        <div className="data5">
+          <Icon type="exclamation-circle" />
+          <p>{errorMessage}</p>
+        </div>
+      )}
+      <div className="bottomBar">
+        <ButtonPrimary
+          text={getMessage('report.fragments.pagination.back')}
+          onClick={goToPreviousPage}
+        />
+        <p>
+          {getMessage('report.fragments.pagination.current')}
+          {currentPage}
+          {getMessage('report.fragments.pagination.total')}
+          {totalPages}
+        </p>
+        <ButtonPrimary
+          text={getMessage('report.fragments.pagination.next')}
+          onClick={goToNextPage}
+        />
+      </div>
+    </div>
+  );
 };
 
 FragmentLogs.propTypes = {
-  fragments: PropTypes.arrayOf(
-    PropTypes.shape({
-      fragment_id: PropTypes.string.isRequired,
-      received_at: PropTypes.string.isRequired,
-      last_updated_at: PropTypes.string.isRequired,
-      status: PropTypes.string.isRequired // TODO: change to include object
-    })
-  ),
-  currentPage: PropTypes.number.isRequired,
-  totalPages: PropTypes.number.isRequired,
-  onSearch: PropTypes.func.isRequired,
-  onBackPage: PropTypes.func.isRequired,
-  onNextPage: PropTypes.func.isRequired
+  nodeAddress: PropTypes.string.isRequired
 };
 
 export default FragmentLogs;
--- electron-app/src/components/molecules/ReportTool/FragmentLogs/TableFragments.jsx
+++ electron-app/src/components/molecules/ReportTool/FragmentLogs/TableFragments.jsx
@@ -1,38 +1,41 @@
 import React from 'react';
-import { Icon } from 'antd';
+import { Table } from 'antd';
 import PropTypes from 'prop-types';
 import { getMessage } from '../../../../utils/messages';
+import './_style.scss';
 
 const TableFragments = ({ fragments }) => {
-  const renderData = column =>
-    fragments.map(fragment => <p>{fragment[column]}</p>);
+  const columns = [
+    {
+      title: getMessage('report.fragments.columns.fragment'),
+      dataIndex: 'fragmentId',
+      key: 'fragmentId'
+    },
+    {
+      title: getMessage('report.fragments.columns.received'),
+      dataIndex: 'receivedAt',
+      key: 'receivedAt'
+    },
+    {
+      title: getMessage('report.fragments.columns.updated'),
+      dataIndex: 'updatedAt',
+      key: 'updatedAt'
+    },
+    {
+      title: getMessage('report.fragments.columns.status'),
+      dataIndex: 'status',
+      key: 'status'
+    }
+  ];
 
   return (
-    <div>
-      <div className="data4">
-        <div className="col">
-          <h4>{getMessage('report.fragments.columns.fragment')}</h4>
-          {fragments && fragments.length > 0 && renderData('fragment_id')}
-        </div>
-        <div className="col">
-          <h4>{getMessage('report.fragments.columns.received')}</h4>
-          {fragments && fragments.length > 0 && renderData('received_at')}
-        </div>
-        <div className="col">
-          <h4>{getMessage('report.fragments.columns.updated')}</h4>
-          {fragments && fragments.length > 0 && renderData('last_updated_at')}
-        </div>
-        <div className="col">
-          <h4>{getMessage('report.fragments.columns.status')}</h4>
-          {fragments && fragments.length > 0 && renderData('status')}
-        </div>
-      </div>
-      {(!fragments || !fragments.length > 0) && (
-        <div className="data5">
-          <Icon type="exclamation-circle" />
-          <p>{getMessage('report.fragments.missing')}</p>
-        </div>
-      )}
+    <div className="tabla">
+      <Table
+        columns={columns}
+        dataSource={fragments}
+        pagination={false}
+        size="small"
+      />
     </div>
   );
 };
@@ -44,10 +47,10 @@ TableFragments.defaultProps = {
 TableFragments.propTypes = {
   fragments: PropTypes.arrayOf(
     PropTypes.shape({
-      fragment_id: PropTypes.string.isRequired,
-      received_at: PropTypes.string.isRequired,
-      last_updated_at: PropTypes.string.isRequired,
-      status: PropTypes.string.isRequired // TODO: change to include object
+      fragmentId: PropTypes.string.isRequired,
+      receivedAt: PropTypes.string.isRequired,
+      updatedAt: PropTypes.string.isRequired,
+      status: PropTypes.string.isRequired
     })
   )
 };
