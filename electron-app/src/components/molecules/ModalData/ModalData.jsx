3d986a6954ba2bef9f5abfbb63725ebee0c2a7c6
aalleva@atixlabs.com <aalleva@atixlabs.com>
2019-12-03 10:51:08 -0300

    create resizable modal

--- electron-app/src/components/molecules/ModalData/ModalData.jsx
+++ electron-app/src/components/molecules/ModalData/ModalData.jsx
@@ -1,12 +1,29 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 import { Modal } from 'antd';
+import { ResizableBox } from 'react-resizable';
 import './_style.scss';
 
 const ModalData = ({ visible, onOk, title, content, topic, onCancel }) => (
-  <Modal title={topic} visible={visible} onOk={onOk} onCancel={onCancel} maskClosable cancelButtonProps={{style:{display:'none'}}}>
-    <h2 className="TitleModal">{title}</h2>
-    <p>{content}</p>
+  <Modal
+    title={topic}
+    visible={visible}
+    onOk={onOk}
+    onCancel={onCancel}
+    maskClosable
+    cancelButtonProps={{ style: { display: 'none' } }}
+  >
+    <ResizableBox
+      //width={300}
+      height={200}
+      //minConstraints={[100, 100]}
+      //maxConstraints={[900, 900]}
+    >
+      <div className="resizable-content">
+        <h2 className="TitleModal">{title}</h2>
+        <p>{content}</p>
+      </div>
+    </ResizableBox>
   </Modal>
 );
 
